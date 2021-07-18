67d9fc663a3b41b54bf98fd1fc6f02a7783b4bd0
dawidurbanski <hi@dawidurbanski.com>
2021-07-18 12:17:27 +0200

    Scaffold project from template

--- index.js
+++ index.js
@@ -2,9 +2,51 @@
 
 'use strict';
 
-var fs = require('fs');
-var dir = process.cwd() + '/tmp';
+const { exec } = require('child_process');
+const fse = require('fs-extra');
+const { default: slugify } = require('slugify');
+const Handlebars = require("handlebars");
 
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
+const args = process.argv.slice(2);
+
+const name = args[0];
+const slug = slugify(name.toLowerCase());
+
+const dir = `${process.cwd()}/${slug}`;
+
+console.log(__dirname);
+
+if (!fse.existsSync(dir)){
+    fse.mkdirSync(dir);
 }
+
+fse.copySync(`${__dirname}/template/src`, `${dir}/src`);
+
+const templateData = { name, slug };
+
+const templates = [
+    {
+        file: 'index.html',
+        path: 'index.html.hbs'
+    },
+    {
+        file: 'package.json',
+        path: 'package.json.hbs'
+    }
+];
+
+for (const { file, path } of templates) {
+    fse.readFile(`${__dirname}/template/${path}`, 'utf8', (err, data) => {
+        const template = Handlebars.compile(data);
+        fse.writeFileSync(`${dir}/${file}`, template(templateData));
+    });
+}
+
+exec('npm install', {cwd: dir}, (err, stdout, stderr) => {
+    if (err) {
+        return;
+    }
+
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+});
---
+++ template/src/scripts/index.js
@@ -0,0 +1,9 @@
+/**
+ * PUT YOUR JAVASCRIPT HERE
+ * YOU CAN REMOVE ALL CONTENTS OF THIS FILE
+ * IT'S JUST AN EXAMPLE
+ */
+
+window.addEventListener('DOMContentLoaded', () => {
+    console.log('Hello!');
+});
